b1da2cc7d1a1ef350806e62a34a1c2306ae64624
Pascal Horton <pascal.horton@giub.unibe.ch>
2023-03-29 14:56:23 +0200

    Fixing css issue.

--- docs/conf.py
+++ docs/conf.py
@@ -93,7 +93,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_css_files = ['custom.css']
+html_css_files = ['css/custom.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
